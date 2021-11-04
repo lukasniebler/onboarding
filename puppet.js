2d9f983fed21b6e66960cbadbff735c4a482377c
Lukas Niebler <nieblerlukas@gmail.com>
2021-11-04 09:26:27 +0100

    forEach Loop

--- puppet.js
+++ puppet.js
@@ -1,27 +1,44 @@
 const puppeteer = require('puppeteer')
+const datensatzInstanzen = [
+    'https://testinstanz-natfak:8890',
+    'https://wp-plugin-lorem-ipsum:8890',];
 
-async function getVisual() {
-	try {
-		const URL = 'https://testinstanz-natfak:8890';
-		const browser = await puppeteer.launch({
-            slowMo: 250,
-        });
-		const page = await browser.newPage();
+datensatzInstanzen.forEach(item => {
+    async function getVisual() {
+        try {
+            const URL = item;
+            const browser = await puppeteer.launch({
+                slowMo: 250,
+            });
+            const page = await browser.newPage();
+    
+            await page.setViewport({
+                width: 1920,
+                height: 1080,
+                deviceScaleFactor: 1,
+            });
+            await page.goto(URL);
+            await page.screenshot({ path: `./Data/${item.substring(7)}_screenshot_1920.png` }); //Später Länderkürzel noch optimieren
+            const h1 = await page.$eval("h1", el => el.innerText);
+            console.log(h1);
+    
+            await browser.close();
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    
+    getVisual()
+});
 
-        await page.setViewport({
-            width: 1920,
-            height: 1080,
-            deviceScaleFactor: 1,
-        });
-		await page.goto(URL);
-		await page.screenshot({ path: './Data/screenshot.png' });
-        const h1 = await page.$eval("h1", el => el.innerText);
-        console.log(h1);
+/*
 
-		await browser.close();
-	} catch (error) {
-		console.error(error)
-	}
-}
 
-getVisual()
+/**
+ * Aufgaben...
+ * - a11y
+ * - h1 & h2
+ * - Bilder Alt-Texte
+ * - Screenshot
+ * - 
+ */
