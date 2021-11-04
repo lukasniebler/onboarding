4e385285aa3da8e42009703155ea366f7fdc8590
Lukas Niebler <nieblerlukas@gmail.com>
2021-11-04 09:04:59 +0100

    Base

--- puppet.js
+++ puppet.js
@@ -2,14 +2,23 @@ const puppeteer = require('puppeteer')
 
 async function getVisual() {
 	try {
-		const URL = 'https://www.lms.tf.fau.de'
-		const browser = await puppeteer.launch()
-		const page = await browser.newPage()
+		const URL = 'https://www.wi1.rw.fau.de';
+		const browser = await puppeteer.launch({
+            slowMo: 250,
+        });
+		const page = await browser.newPage();
 
-		await page.goto(URL)
-		await page.screenshot({ path: 'screenshot.png' })
+        await page.setViewport({
+            width: 1920,
+            height: 1080,
+            deviceScaleFactor: 1,
+        });
+		await page.goto(URL);
+		await page.screenshot({ path: './Data/screenshot.png' });
+        const h1 = await page.$eval("h1", el => el.innerText);
+        console.log(h1);
 
-		await browser.close()
+		await browser.close();
 	} catch (error) {
 		console.error(error)
 	}
